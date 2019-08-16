bdb45ae522e159c1b6002d9230005bd0150c3084
AlexMartinezA <53943737+AlexMartinezA@users.noreply.github.com>
2019-08-16 10:00:41 -0400

    Add files via upload

---
+++ AREA DEL CUADRADO.cpp
@@ -0,0 +1,19 @@
+// AREA DEL CUADRADO.cpp : Defines the entry point for the console application.
+//Autor:Alex Martínez Avendaño
+
+#include "stdafx.h"
+#include <iostream>
+#include "conio.h"
+
+using namespace std;
+
+void main()
+{
+	float área,a;
+	cout<<"ingrese el valor de a: ";
+	cin>>a;
+	cout<<"el área del cuadrado es: ";
+	área=a*a;
+	cout<<área;
+	getch();
+}
